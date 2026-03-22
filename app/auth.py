752be768c352f176d91a153fb4aae574dce14b80
kev <knvasquez@mail.fhsu.edu>
2026-03-22 17:35:28 -0500

    imported sqlite 3 and functools. This is for authentication. Also, required register/login system and GET and POST handling.

---
+++ app/auth.py
@@ -0,0 +1,93 @@
+# INF601 - Advanced Programming in Python
+
+# Kevin Vasquez
+
+# Mini Project 3
+
+import sqlite3
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from .db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            db = get_db()
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+                return redirect(url_for("auth.login"))
+            except sqlite3.IntegrityError:
+                error = f"User {username} is already registered."
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+        db = get_db()
+        error = None
+
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("tasks.dashboard"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+    return wrapped_view
