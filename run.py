f94ff3a4f0fe88a4dd805f0cb9c2e8743fc90142
kev <knvasquez@mail.fhsu.edu>
2026-03-22 16:22:33 -0500

    Add Flask app entry point and run configuration.

---
+++ run.py
@@ -0,0 +1,12 @@
+# INF601 - Advanced Programming in Python
+
+# Kevin Vasquez
+
+# Mini Project 3
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
